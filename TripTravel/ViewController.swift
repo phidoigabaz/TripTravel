00d66f767e295bb67db25665df2e736193f86de5
Tạ Thành Đạt <dattt@gemvietnam.com>
2019-05-20 15:52:04 +0700

    Initial Commit

---
+++ TripTravel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripTravel
+//
+//  Created by Tạ Thành Đạt on 5/20/19.
+//  Copyright © 2019 dattt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
